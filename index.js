accc9b443d7c20a3320f5e1ad23e4b58d0988579
Felipe Najson <felipe.najson@gmail.com>
2023-09-19 11:19:03 -0300

    specifying node and npm versions

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ import app from './src/app.js'
 const PORT = process.env.PORT ?? 1234
 
 app.listen(PORT, () => {
-  console.log(`server listening on port http://localhost:${PORT}`)
+  console.log(`server listening on port ${PORT}`)
 })
